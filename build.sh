ef999e0f624d23eba22c28fd05c5539d81140dde
Marcus Olsson <olsson.e.marcus@gmail.com>
2019-12-16 11:20:30 +0100

    Refactor

--- build.sh
+++ build.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-TUTORIALS_DIR="tutorial"
+TUTORIALS_DIR="tutorials"
 OUTPUT_DIR="public"
-LANDING_TEMPLATE="layout/landing.html"
-CODELAB_TEMPLATE="layout/codelab.html"
-STYLES_DIR="layout/assets"
+
+TEMPLATES_DIR="templates"
 
 # Create output directory if it doesn't exist.
 [ ! -d "${OUTPUT_DIR}" ] && mkdir -p "${OUTPUT_DIR}"
 
 # Build CSS.
-sass "${STYLES_DIR}/google-codelab.scss" "${OUTPUT_DIR}/google-codelab.css"
+sass "${TEMPLATES_DIR}/google-codelab.scss" "${OUTPUT_DIR}/google-codelab.css"
 
 # Build tutorials.
-pushd tutorial # Needs to run in the directory with the tutorials.
-claat export -f "../${CODELAB_TEMPLATE}" -o "../${OUTPUT_DIR}" *.md
+pushd "${TUTORIALS_DIR}" # Needs to run in the directory with the tutorials.
+claat export -f "../${TEMPLATES_DIR}/codelab.html" -o "../${OUTPUT_DIR}" *.md
 popd
 
 # Build landing page
 go run cmd/claat-landing/main.go  \
-    -template "${LANDING_TEMPLATE}"         \
+    -template "${TEMPLATES_DIR}/landing.html"         \
     -codelabs-dir "${TUTORIALS_DIR}" \
     -output-dir "${OUTPUT_DIR}"
 
-cp "${STYLES_DIR}/landing.css" "${OUTPUT_DIR}"
+cp "${TEMPLATES_DIR}/landing.css" "${OUTPUT_DIR}"
